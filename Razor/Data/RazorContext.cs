7c9684cd9d0fde088590a59d117cb1adb300e8c8
djChek <djandrey2011@gmail.com>
2020-07-10 14:04:33 +0300

    initial commit

---
+++ Razor/Data/RazorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Razor.Models;
+
+namespace Razor.Data
+{
+    public class RazorContext : DbContext
+    {
+        public RazorContext (DbContextOptions<RazorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Razor.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Razor/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Razor.Models
+{
+    public class Movie
+    {
+   
+        public int ID { get; set; }
+        [StringLength(60, MinimumLength =3)]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        public string Genre { get; set; }
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "Decimal(18,2)")]    
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'s-]*&")]
+        public string Rating { get; set; }
+    }
+}
